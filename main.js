aa7c8f99ce8c00c541b02805f81e6a932b5c860f
M-Enes <eneskaracap@gmail.com>
2025-03-16 17:39:43 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+var examslist = document.getElementById('examslist')
+
+var exams = [
+    { name: 'Physics 2', date: new Date(2025, 2, 18, 17, 0, 0) },
+    { name: 'Calculus 2', date: new Date(2025, 2, 20, 17, 0, 0) },
+    { name: 'Programming Languages', date: new Date(2025, 2, 20, 13, 0, 0) },
+    { name: 'Data Structures', date: new Date(2025, 2, 24, 11, 0, 0) },
+]
+
+exams.forEach(exam => {
+    var li = document.createElement('li')
+    var minutes_left = (((exam.date - new Date()) / 60000) % 60).toFixed(0)
+    var hours_left = ((exam.date - new Date()) / 3600000).toFixed(0)
+    if (exam.date < new Date()) {
+        minutes_left = hours_left = 0;
+    }
+    li.textContent = exam.name + ' - Time left: ' + hours_left + ' hours - ' + minutes_left + ' minutes - '+ exam.date.toLocaleString()
+    li.style.color = exam.date < new Date() ? 'gray' : 'black'
+    li.style.fontWeight = exam.date < new Date() ? 'normal' : 'bold'
+    li.style.textDecoration = exam.date < new Date() ? 'line-through' : 'none'
+    li.style.fontSize = exam.date < new Date() ? '1em' : '3em'
+
+    examslist.appendChild(li)
+})
+
+
